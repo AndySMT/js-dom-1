f3b399f18c2176cb1938c0540ee3e47f07ec0f5e
Andy Simota <andyiulian.simota@gmail.com>
2024-11-07 15:00:34 +0100

    created onOff function and added ID at section elements

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-/* *Consegna*
-Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
-Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
-*BONUS*:100:
-Facciamo accendi e spegni:
-- Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
-- Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
-- E così via...
-Per fare questo bonus potremmo aver bisogno di del metodo string.includes() :occhiolino:
- */
-
-let on_off = document.querySelector("button");
-on_off.addEventListener("click");
-/* on_off = "img/yellow_lamp"; */
+let on_off = document.getElementById("btn");
+let image = document.getElementById("image");
+on_off.addEventListener("click", onOff);
+function onOff() {
+  if (image.src.includes("white_lamp")) {
+    image.src = "img/yellow_lamp.png";
+    image.alt = "Off";
+    on_off.innerHTML = "Spegni";
+  } else {
+    image.src = "img/white_lamp.png";
+    image.alt = "On";
+    on_off.innerHTML = "Accendi";
+  }
+}
