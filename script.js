9df1c265d31d0b7aeaf8465cd656a500f0e2e2ad
Andy Simota <andyiulian.simota@gmail.com>
2024-11-08 10:00:25 +0100

    fixed lamp rotation

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let on_off = document.getElementById("btn");
 let image = document.getElementById("image");
+let background = document.querySelector(".container");
 on_off.addEventListener("click", onOff);
 function onOff() {
+  background.classList.toggle("light");
   if (image.src.includes("white_lamp")) {
     image.src = "img/yellow_lamp.png";
     image.alt = "Off";
